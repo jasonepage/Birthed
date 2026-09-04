76acba9cfbc1abe257d5a5dc5e56e222a5e5e147
jasonepage <72579902+jasonepage@users.noreply.github.com>
2026-09-04 14:57:48 -0700

    Initial Commit

---
+++ Birthed/BirthedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthedApp.swift
+//  Birthed
+//
+//  Created by Jason Page on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
